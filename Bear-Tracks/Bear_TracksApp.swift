f8e9860956f1ee71411bae33ae83a42a2cc7aef3
Surabhi Bachhav <sb2673@cornell.edu>
2024-12-06 16:18:56 -0500

    Initial Commit

---
+++ Bear-Tracks/Bear_TracksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bear_TracksApp.swift
+//  Bear-Tracks
+//
+//  Created by Surabhi Bachhav on 12/6/24.
+//
+
+import SwiftUI
+
+@main
+struct Bear_TracksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
